12094d2d8a056b12450368f7023cebe65ccf9dc8
Hafsa <hafsa@Hafsas-MacBook-Air.local>
2023-08-29 16:32:54 +0600

    two problems of graph in eolymp

--- eolymp/floyd.cpp
+++ eolymp/floyd.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 100;
+const int INF = 1e9+7;
 int main()
 {
     int n; cin>>n;
-    int dist[n+1][n+1];
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){  
-            if(i==j) dist[i][j] = 0;
-            else dist[i][j] = INF;
-        }
-    }
+    int dist[n][n];
+    
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             cin>>dist[i][j];
@@ -19,18 +14,27 @@ int main()
             }
         }
     }
-    int mx = INT_MIN;
+   
     for(int k = 0; k<n; k++){
         for(int i = 0; i<n; i++){
            for(int j = 0; j<n; j++){
-            if(dist[i][k] != INF && dist[k][j] != INF){
+            if(dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j]<dist[i][j]){
                 dist[i][j] = min(dist[i][j],dist[i][k]+dist[k][j]);
-                mx = max(mx, dist[i][j]);
+                
             }
            } 
         }
     }
     
+    int mx = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] != INF) {
+                mx = max(mx, dist[i][j]);
+            }
+        }
+    }
+
     cout<<mx<<endl;
     
     return 0;
--- eolymp/floyd_1.cpp
+++ eolymp/floyd_1.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int INF = 100;
+const int INF = 1e9+5;
 int main()
 {
     int n; cin>>n;
-    int dist[n+1][n+1];
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){  
-            if(i==j) dist[i][j] = 0;
-            else dist[i][j] = INF;
-        }
-    }
+    int dist[n][n];
+    
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             cin>>dist[i][j];
